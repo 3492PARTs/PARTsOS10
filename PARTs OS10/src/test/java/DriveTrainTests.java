0a70b539638d1777ec4924e1ac9a370022d97d0d
Camtriggerhappy <ckiw23@gmail.com>
2020-08-14 01:17:18 -0400

    Tests can now build and execute without error

--- PARTs OS10/src/test/java/DriveTrainTests.java
+++ PARTs OS10/src/test/java/DriveTrainTests.java
@@ -1,7 +1,11 @@
-import frc.robot.subsystems.DriveSparkMax;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import testClasses.DriveSparkMax;
+
+import  org.junit.Assert.*;
+
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 
@@ -10,19 +14,31 @@ public class DriveTrainTests {
     @Test
     public  void move(){
 
-        DriveSparkMax driveSparkMax = (DriveSparkMax) mock(DriveSparkMax.class);
+        DriveSparkMax driveSparkMax = new DriveSparkMax();
 
-            double[] expected = {1,1};
+            double[] expected = {1.0,1.0};
             driveSparkMax.move(1.0,1.0);
+            double[] speeds = driveSparkMax.getSpeeds();
 
-            assertEquals( driveSparkMax.getSpeeds()[0],expected[0],.01);
 
-            assertEquals( driveSparkMax.getSpeeds()[1],expected[1],.01);
+            assertEquals(expected[0], speeds[0],.02);
+
+            assertEquals(expected[1], speeds[1], 02);
     }
 
     @Test
-    public void adding(){
-        assertEquals(1,1,112);
+    public void stop(){
+        DriveSparkMax driveSparkMax = new DriveSparkMax();
+        for (double i = 0; i < 1.01 ; i+=.01) {
+
+            driveSparkMax.move(i,i);
+
+            driveSparkMax.stop();
+
+            assertEquals(0.0, driveSparkMax.getSpeeds()[0], 0);
+            assertEquals(0.0, driveSparkMax.getSpeeds()[1], 0);
+
+        }
     }
     
 }
